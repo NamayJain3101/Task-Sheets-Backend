71990cf52530e9adc0d720cf65b9fa9145510d48
namayjain3101 <namayjain.jainnamay@gmail.com>
2022-03-13 10:32:25 +0530

    bug fix

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
-import { admin, protect } from '../Middlewares/authMiddleware.js'
+import { admin, protect } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
